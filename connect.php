576de6d78a9bd46c5749d4106daa38aae4531ce9
Serge <Serge.su@hwtco.com>
2021-10-25 16:31:57 +0800

    測試A_repo

--- connect.php
+++ connect.php
@@ -3,12 +3,13 @@ $serverName = "LAPTOP-9M82HBCJ"; //serverName\instanceName
 
 // Since UID and PWD are not specified in the $connectionInfo array,
 // The connection will be attempted using Windows Authentication.
-$connectionInfo = array( "Database"=>"testdb");
-$conn = sqlsrv_connect( $serverName, $connectionInfo);
+$connectionInfo = array("Database" => "testdb");
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 
-if( $conn ) {
+if ($conn) {
      echo "Connection established.<br />";
-}else{
+} else {
      echo "Connection could not be established.<br />";
-     die( print_r( sqlsrv_errors(), true));
+     die(print_r(sqlsrv_errors(), true));
 }
+// 123
